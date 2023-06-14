622477026a61c1af3f6f960a04e4967e832d7b34
Lichthagel <lichthagel@tuta.io>
2023-06-14 18:25:42 +0200

    setup rome

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import rome from "astro-rome";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), rome()],
 });
