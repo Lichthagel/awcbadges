980cdc68e270e70d77ac60009bc7a984a6d416cd
Lichthagel <lichthagel@tuta.io>
2023-06-20 00:27:29 +0200

    index page

--- src/models.ts
+++ src/models.ts
@@ -7,29 +7,49 @@ const zBadge = z.object({
   status: z.enum(["in-progress", "submitted", "completed"]),
 });
 
-export const zChallenge = z.object({
-  name: z.string(),
-  url: z
-    .string()
-    .url()
-    .regex(/https:\/\/anilist\.co\/forum\/thread\/([0-9]+)\/comment\/([0-9]+)/)
-    .transform((url) => {
-      const match = url.match(
+export type Badge = z.infer<typeof zBadge>;
+
+const getBadge = (badge: Badge | Badge[]): Badge & { name?: string } => {
+  if (Array.isArray(badge)) {
+    const result = badge.filter((b) => b.status === "completed").at(-1);
+
+    // rome-ignore lint/style/noNonNullAssertion: <explanation>
+    return result ?? badge.at(-1)!;
+  } else {
+    return badge;
+  }
+};
+
+export const zChallenge = z
+  .object({
+    name: z.string(),
+    url: z
+      .string()
+      .url()
+      .regex(
         /https:\/\/anilist\.co\/forum\/thread\/([0-9]+)\/comment\/([0-9]+)/,
-      );
+      )
+      .transform((url) => {
+        const match = url.match(
+          /https:\/\/anilist\.co\/forum\/thread\/([0-9]+)\/comment\/([0-9]+)/,
+        );
 
-      if (!match) {
-        throw new Error("Invalid URL");
-      }
+        if (!match) {
+          throw new Error("Invalid URL");
+        }
 
-      return {
-        thread: parseInt(match[1]),
-        comment: parseInt(match[2]),
-        full: url,
-      };
-    }),
-  badge: zBadge.or(z.array(zBadge.extend({ name: z.string() })).nonempty()),
-});
+        return {
+          thread: parseInt(match[1]),
+          comment: parseInt(match[2]),
+          full: url,
+        };
+      }),
+    badge: zBadge.or(z.array(zBadge.extend({ name: z.string() })).nonempty()),
+  })
+  .transform((challenge) => ({
+    ...challenge,
+    focusBadge: getBadge(challenge.badge),
+  }));
 
 export type Challenge = z.infer<typeof zChallenge>;
 
