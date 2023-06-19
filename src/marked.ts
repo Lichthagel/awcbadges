eed16faca9818f99bfd4f8e8c9b9e93580d0c59f
Lichthagel <lichthagel@tuta.io>
2023-06-19 23:02:17 +0200

    styled markdown

--- src/marked.ts
+++ src/marked.ts
@@ -3,8 +3,9 @@ import { marked } from "marked";
 marked.use({
   breaks: true,
   renderer: {
+    paragraph: (text) => `<p class="my-3">${text}</p>`,
     link: (href, title, text) =>
-      `<a href="${href}" target="_blank" rel="noopener noreferrer">${text}</a>`,
+      `<a href="${href}" target="_blank" rel="noopener noreferrer" class="text-blue-500 underline">${text}</a>`,
   },
   mangle: false,
   headerIds: false,
