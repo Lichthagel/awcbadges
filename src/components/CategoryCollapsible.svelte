74e5bdb4a50c5148f29b16b78158cfaa36850c0b
Lichthagel <hey@lichthagel.de>
2024-06-29 23:27:47 +0200

    fix: category remove unneeded span

--- src/components/CategoryCollapsible.svelte
+++ src/components/CategoryCollapsible.svelte
@@ -11,18 +11,23 @@
 </script>
 
 <div>
-  <button class="text-ctp-pink mx-auto my-5 w-max" on:click={() => toggle()} type="button">
-    <span class="inline-block transition-transform align-middle" class:rotate-90={isOpen}>
-      <span class="w-8 h-8 i-lucide-chevron-right" />
-    </span>
+  <button
+    class="text-ctp-pink mx-auto my-5 w-max"
+    on:click={() => toggle()}
+    type="button"
+  >
+    <span
+      class="inline-block transition-transform w-8 h-8 i-lucide-chevron-right"
+      class:rotate-90={isOpen}
+    />
     <span class="text-3xl lowercase align-middle">{title}</span>
   </button>
   {#if isOpen}
-  <div
-    class="overflow-x-scroll whitespace-nowrap lg:whitespace-normal"
-    transition:slide={{ duration: 500 }}
-  >
+    <div
+      class="overflow-x-scroll whitespace-nowrap lg:whitespace-normal"
+      transition:slide={{ duration: 500 }}
+    >
       <slot />
-  </div>
+    </div>
   {/if}
 </div>
