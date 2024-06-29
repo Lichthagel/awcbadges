7811137b242770e5c47dcfb3bffee2b5e371f91a
Lichthagel <hey@lichthagel.de>
2024-06-29 20:54:47 +0200

    feat: collapsible categories

---
+++ src/components/CategoryCollapsible.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { slide } from "svelte/transition";
+
+  export let title: string;
+
+  let isOpen = false;
+
+  const toggle = () => {
+    isOpen = !isOpen;
+  };
+</script>
+
+<div>
+  <button on:click={() => toggle()} type="button">
+    <h2 class="text-gradient mx-auto my-5 w-max text-3xl lowercase">
+      {title}
+    </h2>
+  </button>
+  {#if isOpen}
+  <div
+    class="overflow-x-scroll whitespace-nowrap lg:whitespace-normal"
+    transition:slide={{ duration: 500 }}
+  >
+      <slot />
+  </div>
+  {/if}
+</div>
